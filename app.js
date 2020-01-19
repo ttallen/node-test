dc23f8b0e51fef6601a573cadcff6339118baa22
Allen <asd9fgh9@gmail.com>
2020-01-19 22:47:12 +0800

    指定路徑&網址參數

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
-    res.send('1234');
+app.get('/user/:name/',function(req,res){
+    var myName = req.params.name;
+    var limit = req.query.limit;
+    var q = req.query.q;
+    res.send('<html><head></head><body><p>' 
+            + myName 
+            + '要搜尋關鍵字' 
+            + q 
+            + '且是找前' 
+            + limit 
+            + '筆的資料</p></body></html>');
 })
 
 var port = process.env.PORT || 3000;
-console.log(port);
 app.listen(port);
